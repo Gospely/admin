270123b8e0a9139cedf477013b6d5341cf7209b3
leinue <597055914@qq.com>
2016-09-25 09:21:54 +0800

    update

--- build/dev-server.js
+++ build/dev-server.js
@@ -11,7 +11,7 @@ require('./setup-dev-server')(app)
 app.use('/assets', express.static(resolve('../client/assets')))
 app.use(favicon(resolve('../client/assets/logo@2x.png')))
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8087
 app.listen(port, () => {
   console.log(`server started at localhost:${port}`)
 })
