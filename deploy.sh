e335987acb17586063fa46a6d9a35ba84a9c4e2d
leinue <597055914@qq.com>
2016-10-06 17:26:52 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ sh ./scripts/sync.sh
 #sync
 npm run build
 cd ./dist && git add . && git commit -a -m 'update' && git push -u origin master
-ssh root@119.29.153.143 'docker exec -d gospel_admin git pull'
+#ssh root@119.29.153.143 'docker exec -d gospel_admin git pull'
